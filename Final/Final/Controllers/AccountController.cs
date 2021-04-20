ee34c709561248781957fbe163bdd2c18b9044d8
nerodro <ntarak2002@gmail.com>
2021-04-20 17:46:59 +0300

    Release

---
+++ Final/Final/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Final.ViewModels; // пространство имен моделей RegisterModel и LoginModel
+using Final.Models; // пространство имен UserContext и класса User
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace AuthApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private StudentContext db;
+        public AccountController(StudentContext context)
+        {
+            db = context;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => u.Login == model.Login && u.Password == model.Password);
+                if (user != null)
+                {
+                    await Authenticate(model.Login); // аутентификация
+
+                    return RedirectToAction("dbOutput", "Home");
+                }
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+       
+        private async Task Authenticate(string userName)
+        {
+            // создаем один claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            // создаем объект ClaimsIdentity
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            // установка аутентификационных куки
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ Final/Final/Models/Sample/Students.cs
@@ -0,0 +1,30 @@
+﻿using Final.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beatch.Models.Sample
+{
+    public static class Students
+    {
+        public static void Initialize(StudentContext context)
+        {
+            if (!context.Students.Any())
+            {
+                context.Students.AddRange(
+                    new StudentModel
+                    {
+                        Name = "Илья",
+                        Surname = "Капралов",
+                        Motherland = "Максимович",
+                        Speciality = "Тракторостроительный факультет, он же главный магосфабрикатор",
+                        State = "Учащийся",
+                        Course = "Т-1А"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
